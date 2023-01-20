391253e0f867e3977c889a0ba32c896d172b3e6c
ykmxxi <ykmxxi97@gmail.com>
2023-01-20 18:04:41 +0900

    백준 완전탐색 15651번: N과 M(3)

---
+++ Baekjoon/src/완전탐색/n과m3번/Main.java
@@ -0,0 +1,95 @@
+package 완전탐색.n과m3번;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static int[] selected;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    // Recurrence Function (재귀 함수)
+    // 만약 M 개를 전부 선택 했으면 -> 조건에 맞는 탐색을 한 가지 성공한 것!
+    // 아직 M 개를 고르지 않음 -> k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
+    static void rec_func(int k) {
+        if (k == M + 1) { // M개의 숫자를 다 골랐으면 출력값 생성
+            // selected[1...M] 배열이 새롭게 탐색된 결과
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int candidate = 1; candidate <= N; candidate++) {
+                // k 번째에 candidate 가 올 수 있으면
+                selected[k] = candidate;
+
+                // k+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞는 모든 방법을 찾아줘
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
