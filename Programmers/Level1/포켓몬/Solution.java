029cb0e1c9f6f9747b681d0c47d8ac54a577469a
ykmxxi <ykmxxi97@gmail.com>
2023-04-04 20:55:56 +0900

    프로그래머스 Lv 1: 포켓몬

---
+++ Programmers/Level1/포켓몬/Solution.java
@@ -0,0 +1,24 @@
+package level1.포켓몬;
+
+/* https://school.programmers.co.kr/learn/courses/30/lessons/1845
+    N / 2 마리를 가져가도 좋다
+    포켓몬은 종류에 따라 번호를 붙여 구분
+    같은 종류의 포켓몬은 같은 번호
+    N개 중 N / 2개를 선택할 때 가질 수 있는 최대 가지수
+    정답의 최대치: 10,000 (int)
+*/
+
+import java.util.HashMap;
+
+class Solution {
+	public int solution(int[] nums) {
+		HashMap<Integer, Integer> pocketMon = new HashMap<>();
+		int len = nums.length;
+
+		for (int num : nums) {
+			pocketMon.put(num, 1);
+		}
+
+		return Math.min(pocketMon.size(), len / 2);
+	}
+}
