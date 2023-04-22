c60b3c1d12ecadb5f0959f322425a31e061166e4
ykmxxi <ykmxxi97@gmail.com>
2023-04-22 16:11:44 +0900

    프로그래머스 Lv 1: 콜라 문제

---
+++ Programmers/Level1/콜라문제/Solution.java
@@ -0,0 +1,26 @@
+package level1.콜라문제;
+
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/132267
+    빈 병 a개를 가져다주면 콜라 b병을 주는 마트가 있을 때
+    빈 병 n개를 가져다주면 몇 병을 받을 수 있는지 계산하는 문제
+    보유 중인 빈 병이 a개 미만이면, 추가적으로 빈 병을 받을 수 없다
+    1 <= b < a <= n <= 1,000,000 (int)
+*/
+
+class Solution {
+	public int solution(int a, int b, int n) {
+		int answer = 0;
+
+		// a개를 가져다주면 b병을 준다.
+		while (n >= a) { // 보유 중인 빈 병 n개가 a개 미만이면 종료
+			int cnt = n / a;
+
+			answer += cnt * b; // 콜라 받기
+			n -= cnt * a; // 가져다 준 빈 병 빼기
+			n += cnt * b; // 받은 콜라를 다 마신 후 빈병으로 만들기
+		}
+
+		return answer;
+	}
+}
